77ceca1557be87c238764a044a102cb6b4e8900d
Alena Nemtsova <jelenatbs@gmail.com>
2024-11-18 18:02:37 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log("Hello! This is the version 1.0.0.");
+
+function calculateRPN(expression) {
+    const stack = [];
+    const tokens = expression.split(' ');
+
+    for (let token of tokens) {
+        if (isNaN(token)) {
+            const b = stack.pop();
+            const a = stack.pop();
+
+            switch (token) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(a - b);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(a / b);
+                    break;
+                default:
+                    throw new Error(`Unknown operator: ${token}`);
+            }
+        } else {
+            stack.push(Number(token));
+        }
+    }
+
+    if (stack.length !== 1) {
+        throw new Error('Invalid expression');
+    }
+
+    return stack.pop();
+}
+
+module.exports = calculateRPN;
